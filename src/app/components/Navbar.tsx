69ebf1ddb5e8e8cfe6c35259e9c01f9df9536fe7
MightyDevKnight <takao.kamio@brandcode.dev>
2022-06-21 09:20:02 +0900

    add tokenflow

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,4 +1,6 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { useSelector } from 'react-redux';
+import axios from 'axios';
 import Box from './Box';
 import { Tabs } from '@/constants/Tabs';
 import Stack from './Stack';
@@ -7,9 +9,23 @@ import { NavbarUndoButton } from './NavbarUndoButton';
 import Minimize from '../assets/minimize.svg';
 import useMinimizeWindow from './useMinimizeWindow';
 import IconButton from './IconButton';
+import { IconFolder } from '@/icons';
+import { tokensSelector } from '@/selectors';
 
 const Navbar: React.FC = () => {
   const { handleResize } = useMinimizeWindow();
+  const tokens = useSelector(tokensSelector);
+  const tokenStringify = require('json-stringify-safe');
+  const handleOpenTokenFlowApp = useCallback(() => {
+    axios({
+      method: 'post',
+      url: 'http://localhost:3000/api/hello',
+      data: tokenStringify(tokens),
+    }).then((response) => {
+      console.log(response.status);
+    });
+    window.open('http://localhost:3000');
+  }, []);
 
   return (
     <Box
@@ -33,6 +49,7 @@ const Navbar: React.FC = () => {
         </div>
         <NavbarUndoButton />
       </Stack>
+      <IconButton tooltip="open tokenflow app" onClick={handleOpenTokenFlowApp} icon={<IconFolder />} />
       <Stack direction="row" align="center">
         <IconButton tooltip="Minimize plugin" onClick={handleResize} icon={<Minimize />} />
       </Stack>
